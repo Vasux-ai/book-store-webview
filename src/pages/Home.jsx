92ebadf04224546c6b01d764d4c3d24246b107f1
VasanthPranmalai10 <144995113+VasanthPranmalai10@users.noreply.github.com>
2026-02-02 21:50:44 +0530

    Enhance Recommmened component with Swiper integration for book display; add News component to Home page and update CSS for navigation button visibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Banner } from './Banner'
 import { TopSeller } from './TopSeller'
 import Recommmened from './Recommmened'
+import News from './News'
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
       <Banner />
       <TopSeller />
       <Recommmened />
+      <News/>
     </>
   )
 }
